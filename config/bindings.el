3a97d4225027c98f0d12af825e5b4ac6fd8a3361
Jesse <jesse.eisses@gmail.com>
2015-07-26 16:54:15 +0200

    Initial config

---
+++ config/bindings.el
@@ -0,0 +1,25 @@
+;; Place your bindings here.
+
+;; For example:
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+
+(define-key evil-normal-state-map (kbd "q") nil)
+
+;;; esc quits
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+(require 'evil-leader)
+(evil-leader/set-key
+  "e" 'find-file
+  "b" 'switch-to-buffer
+  "k" 'kill-buffer
+  "g" 'magit-status
+  "p" 'projectile-find-file)
